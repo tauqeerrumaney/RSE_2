0f52fad18432cc120bf0382f86aac7414a2a353e
maxnowa <max.nowaczyk2014@gmail.com>
2024-06-29 17:02:07 +0200

    docs(load_data): add docstrings

--- src/load_data.py
+++ src/load_data.py
@@ -1,11 +1,68 @@
+"""
+Script for loading and converting EEG data.
+
+This script allows EEG data in a .txt file (in a specific format)
+to be loaded, converted into micro Volts based on the input
+resolution and saved into a smaller data format.
+
+Usage:
+    python3 load_data.py infile outfile [-v]
+
+Positional Arguments:
+    infile      Name of the file to load.
+    outfile     Name of the file to save the data in feather format.
+
+Optional Arguments:
+    -v, --verbose  Use only a subset of the data.
+
+Description:
+    The script processes EEG data stored in a .txt file. It expects the file to
+    have the following tab-separated columns:
+    - id: An integer identifier for the record.
+    - event: An integer representing the event code.
+    - device: A string identifier for the device.
+    - channel: A string representing the EEG channel.
+    - code: An integer code for the data type.
+    - size: An integer representing the size of the data.
+    - signal: A comma-separated list of float values representing the EEG signal.
+
+    The script performs the following steps:
+    1. Reads the data from the input file.
+    2. Converts the signal values from raw units to micro Volts using a specified
+       conversion factor.
+    3. Optionally saves a subset of the data if the --verbose flag is provided.
+    4. Saves the processed data into a feather format file for efficient storage.
+
+Modules Required:
+    - pandas
+    - numpy
+    - traceback
+    - argparse
+    - utils (providing logger and get_path functions)
+
+Functions:
+    main(args): Main function to execute the script logic.
+
+Example:
+    python script.py input.txt output.feather
+"""
+
 import pandas as pd
 import numpy as np
 import traceback
 import argparse
 from utils import logger, get_path
 
-
 def main(args):
+    """
+    Main function to load, process, and save EEG data.
+
+    Args:
+        args: Command-line arguments parsed by argparse.
+
+    Returns:
+        None
+    """
     file_path = get_path(args.infile)
 
     # format of MindBigData data set
@@ -40,36 +97,32 @@ def main(args):
     logger.info("Finished reading data")
 
     try:
-        file_name = (args.outfile).split(".")[0]
-        if args.mock:
-            out_path = get_path(file_name & "_MOCK.feather")
+        file_name = args.outfile.split(".")[0]
+        if args.verbose:
+            out_path = get_path(f"{file_name}_MOCK.feather")
             df_mock = df.head(10000)
             df_mock.to_feather(out_path)
             logger.info(f"Mock file saved to {out_path}")
         else:
             # save data in feather format -> smaller
-            out_path = get_path(file_name & "_EPOC.feather")
+            out_path = get_path(f"{file_name}_EPOC.feather")
             df.to_feather(out_path)
             logger.info(f"Raw file saved to {out_path}")
     # TODO: handle different error types independently
     except Exception:
         logger.error("An error occurred: %s", traceback.format_exc())
 
-
 if __name__ == "__main__":
-    # USAGE = "loading the data"  # TODO: make more precise
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         "infile",
         type=str,
         help="name of the file to load",
-        # default="raw_data_EPOC.txt",
     )
     parser.add_argument(
         "outfile",
         type=str,
         help="name of the file to save the data in feather format",
-        # default="raw_data.feather",
     )
     parser.add_argument(
         "--verbose", "-v",
